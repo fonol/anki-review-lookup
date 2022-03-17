c2491be5e29b8f8a1c255479d59204fa875e39b4
fonol <zeller@lysio.de>
2022-03-17 16:15:27 +0100

    first crude highlighting of search result

--- utils.py
+++ utils.py
@@ -12,15 +12,7 @@ def prettify_search_result_html(html: str, query: str) -> str:
     for qt in query_tokens:
         html = re.sub(r"\b%s\b" % re.escape(qt), f"<mark>{qt}</mark>", html, flags=re.I)
 
-    # html_tokens     = [t.lower() for t in re.split("(<[^>]+>| )")html.split() if t]
-    # html_tokens_wr  = []     
 
-    # for token in html_tokens:
-    #     if token in query_tokens:
-    #         html_tokens_wr.append(f"<mark>{token}</mark>")
-    #     else:
-    #         html_tokens_wr.append(token)
-    
     html = re.sub("\x1f(?:\x1f)*", "<hr/>", html)
     html = re.sub(r"(?:<[hb]r/?>\w*)*$", "", html)
 
